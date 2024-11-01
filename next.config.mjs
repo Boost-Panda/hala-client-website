3af92ed5b5bc3b7a53c8245dd55cc946d7e49c0a
Nauroz Ahmad <ahmadnauroz@gmail.com>
2024-11-01 17:10:43 +0500

    Revert "Update next.config.mjs"
    This reverts commit a459f500b1b022a9b9a8a5cf641d1eb1727d9496.

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,13 @@
-// import withMDX from '@next/mdx';
+import withMDX from '@next/mdx';
 
-// const withMDXObj = withMDX();
+const withMDXObj = withMDX();
 
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // Configure `pageExtensions` to include MDX files
-//   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-//   output: 'export',
-//   // Optionally, add any other Next.js config below
-// };
-
 const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   output: 'export',
+  // Optionally, add any other Next.js config below
 };
 
-export default nextConfig;
-
-// export default withMDXObj(nextConfig);
+export default withMDXObj(nextConfig);
